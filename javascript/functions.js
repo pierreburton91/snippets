14af1916b9eb4387ebed76ed3d06f3a1642551fc
Pierre Burton <pierre.burton91@gmail.com>
2021-04-15 08:44:41 +0200

    add isMac detector

--- javascript/functions.js
+++ javascript/functions.js
@@ -4,3 +4,6 @@ const isStandalone = () => {
 const isiOS = () => {
   return /iphone|ipad|ipod/.test(window.navigator.userAgent.toLowerCase());
 };
+const isMac = () => {
+  return /macintosh/.test(window.navigator.userAgent.toLowerCase());
+}
